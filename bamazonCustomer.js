590ace6d5ffb39492ec4d0393c75c54e382fe41e
Shawn Feiz <shawnfeiz@gmail.com>
2018-02-18 13:39:05 -0600

    function updates.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -56,7 +56,6 @@ function showProducts() {
 //===================================================================
 
 function begin() {
-
 // The first should ask them the ID of the product they would like to buy.
 // The second message should ask how many units of the product they would like to buy.
     inquirer.prompt([
