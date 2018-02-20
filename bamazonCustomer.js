b3339cf3e384b258c03efb8c624c7f6037d4acc8
Shawn Feiz <shawnfeiz@gmail.com>
2018-02-20 15:36:46 -0600

    Formatting update. Working on depleting stock quantitiy.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -49,12 +49,17 @@ function showProducts() {
     connection.query("SELECT item_id, product_name, price FROM products", function(err, results) {
     if (err) throw err;
 
-    console.log(`
-    ID: ${results.item_id}\n
-    Name: ${results.product_name}\n
-    Price: ${results.price}\n`);
 
-    // console.log(results);
+    // console.log(`Item Id Product Price`);
+    // console.log(`${results[0].item_id} ${results[0].product_name} $${results[0].price}`);
+    // console.log(`${results[1].item_id} ${results[1].product_name} $${results[1].price}`);
+    // console.log(`${results[2].item_id} ${results[2].product_name} $${results[2].price}`);
+
+    results.forEach(function(e){
+        console.log(`${e.item_id} ${e.product_name} $${e.price}\n----------------------\n`);
+    })
+
+
     begin();
     });
 }
@@ -68,7 +73,7 @@ function begin() {
         {
             name: "askID",
             type: "input",
-            message: "Interested in an item? Enter the ID of the product you would like to purchase: "
+            message: chalk.green("Interested in an item? Enter the ID of the product you would like to purchase: ")
         },
         {
             name: "units",
@@ -76,13 +81,27 @@ function begin() {
             message: "How many would you like to buy? "
         }
     ]).then(function(answer){
-        console.log(answer.askID);
-        console.log(answer.units);
-
+        connection.query("SELECT * FROM products", function(err, results) {
+        // console.log(answer.askID);
+        // console.log(answer.units);
         //if the quanity wanted is less than the database stock amount then we're good to proceed
         //else we have to alert the user that there is 'Insufficient quanitity!'
+        let newUnits = parseInt(answer.units);
+       
+        if(newUnits > results[newUnits - 1].stock_quantity){
+            console.log('Insufficient quantity!');
+          } else {
+              var newStockQuantity = results[newUnits - 1].stock_quantity - newUnits;
+              console.log(newStockQuantity);
+          }
+
+        // console.log(results[newUnits - 1].stock_quantity);
+
+        // console.log(typeof(test));
 
 
+
+         });
     })
 }
 //===================================================================
